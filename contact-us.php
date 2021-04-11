e9e45ff92db7990aeb17401100533a1241379e28
Pooja Patidar <56690803+poojapatidar21@users.noreply.github.com>
2021-04-11 15:10:15 +0530

    Update contact-us.php

--- contact-us.php
+++ contact-us.php
@@ -7,7 +7,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Contact</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="stylesheet.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
 </head>
 
